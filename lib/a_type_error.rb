5130ca6087bf88cd94d9e6558d0a454d457eed7e
Paula McKinley <pmknyc@optimum.net>
2018-07-18 20:37:09 +0000

    automatically backed up by learn

--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -1,6 +1,10 @@
 def loneliest_1 
-    "1 is the loneliest number"  
-  
-  1 + 1
+  "1 is the loneliest number"  
+  end 
 
+def  not_lonely_2
+  "1+1 = 2 no longer lonely"
+  end
 
+puts loneliest_1
+puts not_lonely_2
